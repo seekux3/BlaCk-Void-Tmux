b862a4e8aef290abe1548dea61ca7497ce2bceb2
alstjr7375 <alstjr7375@daum.net>
2018-04-30 06:02:24 +0900

    some error patch

--- BlaCk-Void-Tmux.sh
+++ BlaCk-Void-Tmux.sh
@@ -1,6 +1,6 @@
 sudo apt-get install xsel tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-git clone https://github.com/black7375/.tmux.git
-ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local .
-tmux source-file ~/.tmux.conf
+git clone https://github.com/black7375/.tmux.git ~/.tmux
+ln -svf .tmux/.tmux.conf ~/
+cp -v .tmux/.tmux.conf.local .
+tmux source ~/.tmux.conf
